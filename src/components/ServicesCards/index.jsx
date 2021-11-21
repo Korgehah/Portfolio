8c9768c4b41fd095ff57fa961737bba08ab532c3
Korgehah <korgehah@gmail.com>
2021-11-21 20:32:02 +0300

    add pf and blog hover effects (react WIP)

--- src/components/ServicesCards/index.jsx
+++ src/components/ServicesCards/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const ServicesCard = ({ src, alt, title, subtitle }) => {
+const ServicesCard = ({ src, alt, title, subtitle, description }) => {
   return (
     <div className='services__card'>
       <img className='services__card-image' src={src} alt={alt} />
       <h2 className='services__card-title'>{title}</h2>
+      <h2 className='services__card-title services__card-title_hidden'>{title}</h2>
       <p className='services__card-subtitle'>{subtitle}</p>
+      <p className='services__card-description'>{description}</p>
+      <a href={src} className='services__card-order'>Order now</a>
     </div>
   );
 };
